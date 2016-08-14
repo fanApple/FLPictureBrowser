bf23e554d90fc398db3cba553417743954b190af
black_锋 <fan_apple1990@163.com>
2016-08-14 22:10:17 +0800

    改动
    做出一点改动，把整体架构修改一下，以便以后的可延展性

---
+++ FLPictureBrowser/Main/Controllers/FLRootViewController.h
@@ -0,0 +1,13 @@
+//
+//  FLRootViewController.h
+//  FLPictureBrowser
+//
+//  Created by __阿彤木_ on 16/8/14.
+//  Copyright © 2016年 http://www.jianshu.com/users/6216a4946b5a/latest_articles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLRootViewController : UIViewController
+
+@end
---
+++ FLPictureBrowser/Main/Controllers/FLRootViewController.m
@@ -0,0 +1,13 @@
+//
+//  FLRootViewController.m
+//  FLPictureBrowser
+//
+//  Created by __阿彤木_ on 16/8/14.
+//  Copyright © 2016年 http://www.jianshu.com/users/6216a4946b5a/latest_articles. All rights reserved.
+//
+
+#import "FLRootViewController.h"
+
+@implementation FLRootViewController
+
+@end
